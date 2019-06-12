41f6217dfbbb0cf535d91d8737f75a263f90184d
Diogo Verissimo <diogodiasverissimo@gmail.com>
2019-06-12 17:50:05 +0100

    Update login-page.php

--- beeogarden/public/login-page.php
+++ beeogarden/public/login-page.php
@@ -16,7 +16,7 @@
         <div id="upper-login-text">
             <div>
                 <img src="img/logo_beeogarden-8.png" alt="Logo">
-                <h1>ENTUK</h1>
+                <h1>ENTUK VAI PO CARALHO MARCO</h1>
             </div>
         </div>
         <div id="login-form">
