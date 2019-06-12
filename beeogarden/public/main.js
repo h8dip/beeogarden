d991b33bd2d25a815286e5b387fc1c059242666c
EmaBonitoCruz <49457368+EmaBonitoCruz@users.noreply.github.com>
2019-06-12 19:56:33 +0100

    Update main.js

--- beeogarden/public/main.js
+++ beeogarden/public/main.js
@@ -8,6 +8,11 @@ window.onload=function(){
     $('#filter-store a').click(function(){
         $('#filter-store a.filter-active').removeClass('filter-active');
         $(this).addClass('filter-active');
+        // $(this).animate({bacgroundColor:"#FBC02D",color:"#fff" })
     });
+
+    // $("#btn1").click(function(){
+    //     $("#box").animate({height: "300px"});
+    //   });
 
 }
