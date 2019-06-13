cdb14ea768f4be424db1e7cd5666266f1bd418cd
Diogo Verissimo <diogodiasverissimo@gmail.com>
2019-06-13 18:49:12 +0100

    ema is n00b

--- beeogarden/public/loja.js
+++
@@ -1,9 +0,0 @@
-window.onload= function(){
-    loja();
-}
-
-function loja(){
-    var lenght_loja = document.getElementsByClassName("produto").length;
-    console.log(lenght_loja);
-    var num_loja= Math.floor(Math.random() * lenght_loja) + 1);
-}
--- beeogarden/public/store-page.php
+++ beeogarden/public/store-page.php
@@ -48,9 +48,6 @@
                 }
             }
         ?>
-        
-
     <script src="main.js"></script>
-    <script src="loja.js"></script>
 </body>
 </html>
