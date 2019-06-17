8d79f5856a4877e13f2048452f5ef2a60069a383
MarcoPereira27 <42180338+MarcoPereira27@users.noreply.github.com>
2019-06-17 23:35:24 +0100

    Cart completo

--- beeogarden/public/cart.php
+++ beeogarden/public/cart.php
@@ -21,11 +21,10 @@
                 <div class="cart-products-img-container">
                     <img src="img/bee-house.PNG" alt="beehouse">
                 </div>
-            </div>
-            <div class="product-cart">
-                <input type="checkbox">
-                <div class="cart-products-img-container">
-                    <img src="img/bee-house.PNG" alt="beehouse">
+                <div class="product-cart-info">
+                    <h3>Casinha para abelhas</h3>
+                    <div>QTD:1</div>
+                    <h2>44,99€</h2>
                 </div>
             </div>
             <div class="product-cart">
@@ -33,9 +32,77 @@
                 <div class="cart-products-img-container">
                     <img src="img/bee-house.PNG" alt="beehouse">
                 </div>
+                <div class="product-cart-info">
+                    <h3>Casinha para abelhas</h3>
+                    <div>QTD:1</div>
+                    <h2>44,99€</h2>
+                </div>
+            </div>
+            <div class="product-cart">
+                <input type="checkbox">
+                <div class="cart-products-img-container">
+                    <img src="img/bee-house.PNG" alt="beehouse">
+                </div>
+                <div class="product-cart-info">
+                    <h3>Casinha para abelhas</h3>
+                    <div>QTD:1</div>
+                    <h2>44,99€</h2>
+                </div>
+            </div>
+            <div class="line">
+                <div class="dotted-line"></div>
             </div>
         </div>
     </div>
+
+    <div id="price-pay">
+        <h2>Preço a pagar</h2>
+        <div id="price-pay-info">
+            <div class="product-buy">
+                <h3>Casinha para abelhas</h3>
+                <h2>44,99€</h2>
+            </div>
+            <div class="product-buy">
+                <h3>Casinha para abelhas</h3>
+                <h2>44,99€</h2>
+            </div>
+            <div class="product-buy">
+                <h3>Casinha para abelhas</h3>
+                <h2>44,99€</h2>
+            </div>
+            <div class="portes-envio">
+                <h3>Portes de Envio</h3>
+                <h2>5,99€</h2>
+            </div>
+            <div class="iva">
+                <h3>IVA</h3>
+                <h2>1,99€</h2>
+            </div>
+            <div class="line">
+                <div class="dotted-line"></div>
+            </div>
+        </div>
+    </div>
+
+    <div id="total">
+        <h2>Total</h2>
+        <div id="total-info">
+            <div class="total-buy">
+                <h3>Total</h3>
+                <h2>52,97€</h2>
+            </div>
+            <div class="line">
+                <div class="dotted-line"></div>
+            </div>
+        </div>
+    </div>
+
+    <div id="buy-btn">
+        <div><h2>CANCELAR</h2></div>
+        <div><h2>FINALIZAR COMPRA</h2></div>
+    </div>
+
+
 
 <script src="main.js"></script>
 </body>
