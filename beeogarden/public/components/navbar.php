48aade1869da6e5ad358375a5ef715c0053ef5e4
EmaBonitoCruz <49457368+EmaBonitoCruz@users.noreply.github.com>
2019-06-21 19:07:55 +0100

    Update navbar.php

--- beeogarden/public/components/navbar.php
+++ beeogarden/public/components/navbar.php
@@ -12,13 +12,13 @@
     <div id="nav-lg">
         <a href="profile-page.php" id="nav-perfil"<?php if($current_page == 'profile'){echo 'class="nav-active"';}?>>Perfil</a>
         <a href="store-page.php" id="nav-loja" <?php if($current_page == 'store'){echo 'class="nav-active"';}?>>Loja</a>
-        <a href="#" id="nav-ranking" <?php if($current_page == 'ranking'){echo 'class="nav-active"';}?>>Ranking</a>
+        <a href="ranking.php" id="nav-ranking" <?php if($current_page == 'ranking'){echo 'class="nav-active"';}?>>Ranking</a>
         <a href="#" id="nav-mapa">Mapa</a>
         <a href="info-page.php" id="nav-info"<?php if($current_page == 'info'){echo 'class="nav-active"';}?>>Info</a>
         <a href="#" id="nav-definicoes">Definições</a>
         <a href="welcome-page.php" id="nav-sair">Sair</a>
     </div>
     <div id="shopping-cart">
-        <i class="fas fa-shopping-cart fa-2x"></i>
+        <a href="cart-page.php"><i class="fas fa-shopping-cart fa-2x"></i></a>
     </div>
 </nav>
