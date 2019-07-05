11b19d29032f85fac2ad330d79ea2143a7ac3704
EmaBonitoCruz <49457368+EmaBonitoCruz@users.noreply.github.com>
2019-07-05 19:36:45 +0100

    404

---
+++ beeogarden/public/404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Thasadith:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="estilos2.css">
+    <link rel="stylesheet" href="estilos3.css">
+    <link href="hamburger.css" rel="stylesheet">
+    <link href="animation.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/9327c61162.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <title>beeogarden | 404</title>
+    <link rel="shortcut icon" href="img/favicon.png" /> 
+</head>
+<body style="overflow:hidden;">
+
+    <div id="container-404">
+        <div id="top-404">
+            <img src="img/404-img.png" alt="">
+        </div>
+        <div id="txt-404">
+            <p>Esta página não existe ou não foi encontrada :(</p>
+        </div>
+        <div id="btn-404">
+            <a href="profile-page.php">Voltar</a>
+        </div>
+
+    </div>
+
+
+
+<script src="main.js"></script>
+    
+</body>
+</html>
